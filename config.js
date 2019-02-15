9b0f163df683e750fca30328f78fd0351974f680
gcho91 <gcho91@gmail.com>
2019-02-15 13:39:01 -0500

    ignore

---
+++ config.js
@@ -0,0 +1,8 @@
+var firebase = {
+    apiKey: "AIzaSyCHw9919VWQNDPnIFZtLagayT0yb25D-7Y",
+    authDomain: "currency-calculator-74b7e.firebaseapp.com",
+    databaseURL: "https://currency-calculator-74b7e.firebaseio.com",
+    projectId: "currency-calculator-74b7e",
+    storageBucket: "",
+    messagingSenderId: "584105934402"
+}
---
+++ src/environments/environment.dist.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "secretkeyhere",
+      authDomain: "currency-calculator-74b7e.firebaseapp.com",
+      databaseURL: "https://currency-calculator-74b7e.firebaseio.com",
+      projectId: "currency-calculator-74b7e",
+      storageBucket: "",
+      messagingSenderId: "584105934402"
+  
+    }
+  };

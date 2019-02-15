3690d6329028b52b2c4ccda4f57433847435abe2
gcho91 <gcho91@gmail.com>
2019-02-15 13:40:02 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCHw9919VWQNDPnIFZtLagayT0yb25D-7Y",
+    apiKey: "secretkeyhere",
     authDomain: "currency-calculator-74b7e.firebaseapp.com",
     databaseURL: "https://currency-calculator-74b7e.firebaseio.com",
     projectId: "currency-calculator-74b7e",
